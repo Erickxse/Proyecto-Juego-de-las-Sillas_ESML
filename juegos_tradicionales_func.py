adc390d408755467b4d6decf1ce18bfd1f65d446
ErickxSebastianMora <96481780+Erickxse@users.noreply.github.com>
2022-12-09 14:37:03 -0500

    Juegos Tradicionales Menu

--- juegos_tradicionales_func.py
+++ juegos_tradicionales_func.py
@@ -12,9 +12,11 @@ Anibal Martin Yucailla Padilla
 Verisión:
 VER.1.1.0
 """
+import numpy as np
 import random as rd
 import time
 import os
+
 
 def menuInicial(juegos):
     """
@@ -106,6 +108,10 @@ def menuPrincipal():
     }
 
     generarMenu(juegos, '4')
+
+def salir():
+    print("Saliendo")
+        
 ##########################################################################################################
 #Funcions para el juego 1 Limbo
 ##########################################################################################################
@@ -366,8 +372,94 @@ def juegoSillas(participantes,sillas):
         print("EL GANADOR ES: ", listaJugadores) #El participante que quede en la lista gana
 
 ##########################################################################################################
-#Funcions para el juego 3 carta escondida
+#Funciones para el juego 3 BUSQUEDA DE PAREJA
 ##########################################################################################################
+
+def tableroparejas(n):
+    
+    """
+    Es un procedimiento que establece una matriz y asigna las posiciones a cada uno de los elementos(fichas-Valores)
+    Parametros:
+    ------------
+        No tiene parametros de entrada
+    
+    Retorna:
+    ------------
+        No retorna ningun valor
+    """
+    #En este apartado calculamos el número de fichas(casillas), de parejas que se podrán poner en el tablero
+    fichasunicas = (n*n)//2
+    
+    #definimos el tamaño del tablero-Matriz que sera n*n, estará formada por una matriz de ceros 
+    #y las fichas a ingresar serán enteros. 
+    tablero = np.zeros(shape=(n,n),dtype =int)
+    
+    #"i", será la ficha a ingresar, la cual tendra una posición de fila y columna
+    
+    i = 1
+      #Establecemos el proceso para que se asignen los valores (números) en las casillas.
+    #El proceso se repetira mientras i sea menor o igual a fichasúnicas. 
+    while i<=fichasunicas:
+        #f1 es la posición en la fila, del primer valor a asignarse aleatoriamente (menor o igual a n) por el programa. 
+        f1 = int(rd.random()*n)+0
+        #c1 es la posición en la Columna, del primer valor a asignarse aleatoriamente (menor o igual a n) por el programa. 
+        c1 = int(rd.random()*n)+0
+        #Para evitar que 2 números caigan en una misma casilla-posición, validamos que:
+        #El tablero en las posiciones f1 y c1, no esten ocupados es decir que no sean iguales a cero.
+        #indicando así que no podemos escribir hasta que se encuentre una casilla vacía. 
+        while not(tablero[f1,c1]==0):
+        #se repite la asignación del primer valor aleatoriamente en f1, debido a que se cumple la condición de no estar ocupado.    
+            f1 = int(rd.random()*n)+0
+        #se repite la asignación del primer valor aleatoriamente en c1, debido a que se cumple la condición de no estar ocupado.    
+            c1 = int(rd.random()*n)+0
+        #establecemos que el valor asignado i, se guardara en la posición fila 1 y columna 1    
+        tablero[f1,c1] = i
+        #f2 es la posición en la fila, del primer valor a asignarse aleatoriamente (menor o igual a n) por el programa. 
+        f2 = int(rd.random()*n)+0
+        #c2 es la posición en la Columna, del primer valor a asignarse aleatoriamente (menor o igual a n) por el programa. 
+        c2 = int(rd.random()*n)+0
+        #Como en el paso anterior para evitar que los siguientes números,caigan en una misma casilla-posición, validamos que:
+        #El tablero en las posiciones f2 y c2, no esten ocupados es decir que no sean iguales a cero.
+        #indicando así que no podemos escribir hasta que se encuentre una casilla vacía.
+        while not(tablero[f2,c2]==0):
+        #se repite la asignación del segundo valor aleatoriamente en f2, debido a que se cumple la condición de no estar ocupado.    
+            f2 = int(rd.random()*n)+0
+        #se repite la asignación del primer valor aleatoriamente en c2, debido a que se cumple la condición de no estar ocupado.    
+            c2 = int(rd.random()*n)+0
+        #establecemos que el valor asignado i, se guardara (escribira) en la posición fila 2 y columna 2    
+        tablero[f2,c2] = i
+    #para continuar llenando toda las casillas, luego de que i se lleo en f1, f2
+    #con la siguiente casilla hasta completar todas o que i sea menor o igual al número de las casillas.
+        i = i + 1
+    #como resultado de todo el proceso se debe devolver la matriz(tablero), 
+    # Cada casilla con sus valores respectivos.
+    return(tablero)
+
+
+def rangoNumero(num):
+    """
+    Es un procedimiento que nos determina si un dato ingresado(numero) esta en rango [0-3]
+    si lo esta retorna un True(Verdadero) en caso de que no lo sea retorna un False(Falso)
+    Parametros:
+    ------------
+        Tiene  un parametro de entrada (num)
+    
+    Retorna:
+    ------------
+        Retorna el valor de True si es el dato ingresado esta dentro del rango [0-3],
+        o un False si no lo esta
+    """
+    #inicializa validadorRangoNumero en falso
+    validadorRangoNumero = False
+    #Si el numero ingresado esta entre [0-3] entonces
+    if (num < 4) and (num >= 0) :
+        #asigna validadorRangoNumero en True
+        validadorRangoNumero = True
+        #retorna validadorRangoNumero
+        return  validadorRangoNumero
+    else:
+        # si no cumple retorna validadorRangoNumero
+        return validadorRangoNumero
 
 
 #JUEGOS
@@ -532,7 +624,8 @@ def juego1():
             #Imprime el fin del juego
             finDeJuego()
         
-    #desarollar Juego 1
+    #desarollar Juego 2    
+
 def juego2():
     """
     Es un procedimiento que ejecuta el juego 2: Juego de las Sillas
@@ -576,14 +669,214 @@ def juego2():
     #Mensaje de numero de participantes y Sillas
     #ingresarNombres(participantes)
     juegoSillas(participantes, sillas)
-    #Llamado a la funcion que desarolla el juego
+ 
+
+  
+ #Llamado a la funcion que desarolla el juego
+ #desarollar Juego 3
+ 
 def juego3():
-    print("Juego 3: Busqueda de Parejas. Escogido")    
-    #desarollar Juego 3
-def salir():
-    print("Saliendo")
+  
+  
+    """
+    Es un procedimiento que ejecuta el juego 3: Juego de Busqueda de pareja
+    Parametros:
+    ------------
+        No tiene parametros de entrada
+    
+    Retorna:
+    ------------
+        No retorna ningun valor
+    """
+    print("Juego 3: Busqueda de Pareja. Escogido")    
+    
+    #Definimos el tamaño del tablero que es de 4 (n=4)
+    n = 4
+    # PROCEDIMIENTO
+    #Para generar el tablero-Matriz llamamos a la función "tableroparejas"
+    tablero = tableroparejas(n)
+    #Iniciamos el juego con el siguiente mensaje
+    print("\n BIENVENIDO VEAMOS COMO ESTA TU MENTE, MEMORIZA LAS POSICIONES PARA JUGAR")
+    #se imprime el tablero con sus las posiciones de las fichas, para que el jugador las memorice
+    print(tablero)
 
+    #Para evitar que el usuario repita el ingreso de las mimas posiciones durante el juego
+    #Establecemos un tablero donde se reflejará las fichas descubiertas de tamaño n*n
+    descubiertas = np.zeros(shape=(n,n),dtype=int)
+    #Inicializamos la variable equivocado,que contará las veces que se equivoca.
+    equivocado = 0
+    #Inicializamos la variable encontrado,que contará las cantidad de aciertos.
+    encontrado = 0
+    #Para realizar varios intentos de encontrar varias parejas usamos While
+    #Como el jugador durante todo el juego solo podrá equivocarse 3 veces, establecemos que:
+    #Se repite los intentos mientras equivocado sea menor a 3 y encontrado sea menor a n*n es decir igual a 16(número de casillas)
+    while (equivocado<3 and encontrado<(n*n)):
+        #Para saber la antidad de descubiertas, imprimimos el siguiente mensaje 
+        print('estado del juego:')
+        #este tablero dará a conocer las fichas con aciertos descubiertas.
+        print(descubiertas)    
+        #Generamos un while para validar datos de entrada de l ficha 1 
+        while True:
+            #Establecemos que el programa intente leer el dato f1, en caso de que no lo consiga
+            #ejecutará, lo que hay en el except
+            try:
+                # Solicitamos que se ingrese la posición en la fila(0 a 3) de la ficha 1.
+                f1 = int(input('ingresa la fila de la  ficha 1:'))
+                #en caso de que con try al intentar leer f1, no fue exitoso, se verifica cuál es el error.
+            except (ValueError,NameError):
+                #dado el caso de que se ingreso caracteres y no números se pide que ingrese un número
+                print("Ingrese un número")
+                #en caso de que si sea un número se continua al siguiente apartado.
+                continue
+        #establecemos un condicional e invocamos a la función "rangoNumero", para 
+        #establecer que si "f1" es verdadero es decir un número menor a 4, como se planteao en la función.
+            if rangoNumero(f1)== True:
 
+                break
+        #caso contrario  
+            else:
+        #si el númeroñ es mayor a 4, se imprime que "la fila debe ser entre 0 y 3"  
+                print("La fila debe ser entre 0 y 3")
+                
+        # resuelto la validación de datos, continuamos con la ejecución del programa.  
+
+        #Generamos un while para validar datos de entrada de la ficha 2
+        while True:
+            #Establecemos que el programa intente leer el dato f1, en caso de que no lo consiga
+            #ejecutará, lo que hay en el except
+            try:
+                # Solicitamos que se ingrese la posición en la fila(0 a 3) de la ficha 2.
+                c1 = int(input('ingresa la columna de la ficha 1 :'))
+                #en caso de que con try al intentar leer f1, no fue exitoso, se verifica cuál es el error.
+            except (ValueError, NameError):
+                #dado el caso de que se ingreso caracteres y no números se pide que ingrese un número
+                print("Ingrese un número")
+                #en caso de que si sea un número se continua al siguiente apartado.
+                continue
+            #establecemos un condicional e invocamos a la función "rangoNumero", para 
+            #establecer que si "f1" es verdadero es decir un número menor a 4, como se planteao en la función.
+            if rangoNumero(c1)==True:    
+                break 
+                #caso contrario  
+            else:
+                #si el número es mayor a 4, se imprime que "la fila debe ser entre 0 y 3"  
+                print("La columna debe ser entre 0 y 3")
+                # resuelto la validación de datos, continuamos con la ejecución del programa.    
+                continue
+        #hacemos un salto al proximo paso del programa.
+
+    
+        #Para evitar que el jugador ingrese posiciones de fichas descubiertas establecemos que:
+        #Podremos ingresar posiciones, mientras esas posiciones no sean aún descubiertas.
+        #Si el jugador ingresa las mismas posiciones , el programa seguirá pidinedo coordenadas de la misma ficha.
+        while not(descubiertas[f1,c1]==0):
+            #si se ingresa posiciones ya descubiertas se imprime el siguiente mensaje
+            print('Esta ficha ya fue descubierta INGRESA OTRAS COORDENADAS')
+            #Dado que se cumple la condición, solicitamos que se ingrese la fila de la ficha 1.
+            f1 = int(input('fila ficha 1:'))
+            #Dado que se cumple la condición, solicitamos que se ingrese la columna de la ficha 1.
+            c1 = int(input('columna ficha 1:'))
+            
+        #Generamos un while para validar datos de entrada de la fila 2 de la ficha 2 
+        while True:
+            #Establecemos que el programa intente leer el dato f2, en caso de que no lo consiga
+            #ejecutará, lo que hay en el except
+            try:
+                # Solicitamos que se ingrese la posición en la fila(0 a 3) de la ficha 2.
+                f2 = int(input('ingresa la fila de la  ficha 2:'))
+                #en caso de que con try al intentar leer f2, no fue exitoso, se verifica cuál es el error. 
+            except (ValueError,NameError):
+                #dado el caso de que se ingreso caracteres y no números se pide que ingrese un número    
+                print("Ingrese un número")
+                #en caso de que si sea un número se continua al siguiente apartado.  
+                continue
+            #establecemos un condicional e invocamos a la función "rangoNumero", para 
+            #establecer que si "f2" es verdadero es decir un número menor a 4, como se planteao en la función.
+            if rangoNumero(f2)== True:
+                break
+            #caso contrario      
+            else:
+                #si el número es mayor a 4, se imprime que "la fila debe ser entre 0 y 3" 
+                print("La fila debe ser entre 0 y 3")
+                # resuelto la validación de datos, continuamos con la ejecución del programa.   
+        #Generamos un while para validar datos de entrada de la columna de la ficha 2    
+        while True:
+            #Establecemos que el programa intente leer el dato c2, en caso de que no lo consiga
+            #ejecutará, lo que hay en el except
+            try:
+                # Solicitamos que se ingrese la posición en la columna(0 a 3) de la ficha 1.
+                c2 = int(input('ingresa la columna de la ficha 2 :'))
+                #en caso de que con try al intentar leer f1, no fue exitoso, se verifica cuál es el error.  
+            except (ValueError, NameError):
+                #dado el caso de que se ingreso caracteres y no números se pide que ingrese un número     
+                print("Ingrese un número")
+            #en caso de que si sea un número se continua al siguiente apartado.  
+                continue
+            #establecemos un condicional e invocamos a la función "rangoNumero", para 
+            #establecer que si "c1" es verdadero es decir un número menor a 4, como se planteao en la función.
+            if rangoNumero(c2)==True:
+                break 
+                #caso contrario   
+            else:
+                #si el número es mayor a 4, se imprime que "la columna debe ser entre 0 y 3" 
+                print("La columna debe ser entre 0 y 3")
+                # resuelto la validación de datos, continuamos con la ejecución del programa.
+        #Para evitar que el jugador ingrese posiciones de fichas descubiertas establecemos que:
+        #Podremos ingresar posiciones, mientras esas posiciones no sean aún descubiertas.
+        #Si el jugador ingresa las mismas posiciones , el programa seguirá pidinedo coordenadas de la misma ficha.
+        while not(descubiertas[f2,c2]==0):
+        #si se ingresa posiciones ya descubiertas se imprime el siguiente mensaje
+            print('Esta ficha ya fue descubierta INGRESA OTRAS COORDENADAS')
+            #Dado que se cumple la condición, solicitamos que se ingrese la fila de la ficha 2.
+            f2 = int(input('fila ficha2:'))
+            #Dado que se cumple la condición, solicitamos que se ingrese la columna de la ficha 2.
+            c2 = int(input('columna ficha2:'))
+            
+        #Se registra que se elige la ficha a descubir de la ficha 1
+        ficha1 = tablero[f1,c1]
+        #  Se registra que se elige la ficha a descubir de la ficha 2 que seria pareja de la ficha 1
+        ficha2 = tablero[f2,c2]
+        
+        #establecemos que si la ficha 1 y ficha 2 descubiertas son iguales. 
+        if ficha1==ficha2:
+            #Utilizamos el tablero descubiertas, para registrar las fichas acertadas por el jugador con la ficha 1.
+            descubiertas[f1,c1] = ficha1
+            #Utilizamos el tablero descubiertas, para registrar las fichas acertadas por el jugador con la ficha 2.
+            descubiertas[f2,c2] = ficha2
+            #Contamos la cantidad de aciertos, todo los aciertos que teniamos mas 2, dado que:
+            #Cada acierto conlleva encontrar 2 fichas.
+            encontrado = encontrado + 2
+            #Se imprime que se encontro una pareja, junto a las fichas descubiertas.
+            print('ENCONTRASTE una pareja..!',ficha1,ficha2)
+            #Usamos else, dado el caso contrario de que las fichas no sean iguales
+            #
+        else:  
+            #Contamos las veces que se equivoco, donde equivocado anteriormente se establecio que inicia en cero y
+            equivocado = equivocado + 1
+        #se imprime que las fichas son diferentes,junto a las fichas descubiertas.
+            print('Las fichas son diferentes: ',ficha1,ficha2)
+
+    # AL FINALIZAR EL JUEGO: 
+    #Se imprime la solución del juego
+    print('\n EL JUEGO HA FINALIZADO')
+    print('Solucion del tablero:')
+    #Se imprime el tablero jugado
+    print(tablero)
+    #Para dar a conocer las fichas descubiertas, se imprime el mensaje:
+    print('Fichas descubiertas:')
+    #Se imprime el tablero con las fichas descubiertas
+    print(descubiertas)
+    #PAra dar a conocer si se encontro toda las fichas o no establecemos que:
+    # si encontrado es igual a todo los aciertos
+    if encontrado==(n*n):
+        #se imprime lo sigueinte felicitando.
+        print(' Muy bien..!! todas las fichas encontradas')
+        #Caso contrario a lo establecido anteeriormente
+    else:
+        #Se imprime lo siguiente:
+        print('Perdiste...  agotaste tus oportunidades...')
+        # Se imprime tambien las fichas encontradas.
+        print('fichas descubiertas:', encontrado)
 if __name__ == '__main__':
     #Variables para juego Limbo
     #Inicializa variables paso de nivel correspondiente al nivel, todas en False
@@ -596,4 +889,9 @@ if __name__ == '__main__':
     #Variables para juego de la silla
     participantes = 0 
     sillas = 0
+    #Variables para juego Busqueda de pareja
+    n=4
+    tablero = tableroparejas(n)
+    equivocado = 0
+    encontrado = 0
     menuPrincipal()
